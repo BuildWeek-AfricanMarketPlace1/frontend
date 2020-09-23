9263640ab1df422ff70722b5281846060456c5a8
Mary Baker <meb601@gmail.com>
2020-09-23 11:56:41 -0400

    Create AddCategory component

---
+++ my-app/src/components/AddCategory.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AddCategory(product) {
+  return;
+  <div>
+    <h2>Add a New Product Category</h2>
+    <input
+      type="text"
+      placeholder="Enter your new category"
+      value={}
+      onChange={}
+    />
+    <h3>Categories:</h3>
+    <h4>{product.catname}</h4>
+  </div>;
+}
+
+export default AddCategory;
---
+++ my-app/src/components/EditDescription.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EditDescription() {
+  return;
+  <div>
+    <h2>Update Description</h2>
+    <input type="text" placeholder={product.description} value={} onChange={} />
+  </div>;
+}
+
+export default EditDescription;
